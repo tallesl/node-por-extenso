4a1be4d228c63c37c8cedbd5389ee9bc97abc711
Talles L <talleslasmar@gmail.com>
2015-07-10 14:31:07 -0300

    fixing mistake of not giving back strings

--- test/non-number.js
+++ test/non-number.js
@@ -2,8 +2,12 @@ var assert     = require('assert')
   , porExtenso = require('../lib/por-extenso')
 
 it('not a number', function () {
+
   assert.equal(porExtenso(undefined), undefined)
-  assert.equal(porExtenso(null), null)
+  assert.equal(porExtenso(null),      null)
+  assert.equal(porExtenso('foo bar'),  'foo bar')
+
   assert(Number.isNaN(porExtenso(NaN)))
+
 })
 
